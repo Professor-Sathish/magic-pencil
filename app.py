834e08b3c066577052409621169a2f1f649af4c7
ProfSathishR <30977778+Professor-Sathish@users.noreply.github.com>
2023-08-01 16:51:54 +0530

    Update app.py

--- app.py
+++ app.py
@@ -39,11 +39,13 @@ def perform_lda(bow_matrix, num_topics):
 # Extract the most relevant words for each topic
 def extract_topic_keywords(lda, vectorizer, num_words=5):
     topic_keywords = []
+    feature_names = vectorizer.get_feature_names()  # Use get_feature_names() instead
     for topic_idx, topic in enumerate(lda.components_):
         top_words_idx = topic.argsort()[-num_words:][::-1]
-        top_words = [vectorizer.get_feature_names_out()[i] for i in top_words_idx]
+        top_words = [feature_names[i] for i in top_words_idx]
         topic_keywords.append(top_words)
     return topic_keywords
+
 
 # Generate theme names based on topic keywords
 def generate_theme_names(topic_keywords):
